e0ca97144084a07ed48ac5706f60dc7d78e84f3d
PishoLu <luhongyu.hi@qq.com>
2019-12-29 23:13:48 +0800

    我这个css一塌糊涂

--- app/djangoapp/frontend/src/components/Index.vue
+++ app/djangoapp/frontend/src/components/Index.vue
@@ -1,26 +1,20 @@
 <template>
   <div id="app">
-    <div>
-    <div class="left_up">
-      <el-input v-model="input" placeholder="请输入内容"></el-input>
-    </div>
-    <div class="left_down">
-      <el-row class="tac">
-        <el-col :span="24">
-          <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
-            background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-            <el-submenu index="1">
-              <template slot="title">
-                <span>好友列表</span>
-              </template>
-              <el-menu-item-group v-for="(item,index) in friends_list" :key="index">
-                <el-menu-item @click="excheng_obj(item.id)">{{item.username}}</el-menu-item>
-              </el-menu-item-group>
-            </el-submenu>
-          </el-menu>
-        </el-col>
-      </el-row>
-    </div>
+    <div class="left">
+      <div class="left_up">
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </div>
+      <div class="left_down">
+        <el-row class="tac">
+          <el-col :span="24">
+            <el-menu default-active="1" default-open="1" class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+                <el-menu-item-group v-for="(item,index) in friends_list" :key="index">
+                  <el-menu-item class="friend-item" @click="excheng_obj(item.id)">{{item.username}}</el-menu-item>
+                </el-menu-item-group>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </div>
     </div>
     <div class="r-up" v-if="!r_up_type">
       <ul id="content" v-for="(item,index) in message_list" :key="index">
@@ -45,23 +39,27 @@
         inputmsg: '',
         friends_list: [{
           "id":23424234,
-          "username":"test"
+          "username":"test",
+          "last_ip":"",
+          "last_port":0
         },{
           "id":23423423,
-          "username":"test2"
+          "username":"test2",
+          "last_ip":"",
+          "last_port":0
         }],
         cruent_obj_id:0,
         // message区域可以分作几类，用于呈现不同的类型的消息。比如查询好友所得的消息，以及聊天记录的消息
         r_up_type:0
       }
     },
+    created: function () {
+      var logging_cookie=this.$cookies.get("logining_userid");
+      // 获取好友列表
+      // 根据好友列表探查好友的存活
+      // 或许可以获取所有的消息记录
+    },
     methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      },
       send_message() {
         this.list.push(this.inputmsg);
         this.inputmsg = ''
@@ -87,33 +85,38 @@
     margin: 0 auto;
   }
 
-  .left_down {
+  .friend-item{
+    width: 200px
+  }
+
+  .left{
     border: 1px solid pink;
     width: 20%;
+  }
+
+  .left_down {
     height: 760px;
     float: left;
     text-align: left;
   }
 
   .left_up{
-    border: 1px solid blue;
     background-color: rgb(71, 79, 85);
     height: 40px;
     text-align: left;
-    width: 20%;
   }
 
   .r-up {
     border: 1px solid yellow;
     width: 79%;
-    height: 400px;
+    height: 600px;
     float: right;
   }
 
   .r-dn {
     border: 1px solid deepskyblue;
     width: 79%;
-    height: 400px;
+    height: 200px;
     float: right;
   }
 
@@ -121,12 +124,13 @@
     resize: none;
     width: 99%;
     border: 1px solid blue;
-    height: 350px;
+    height: 80%;
     float: left;
   }
 
   .sub_button{
     float: right;
+    height: 19%;
   }
 
   ul {
